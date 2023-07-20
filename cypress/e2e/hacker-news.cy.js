5a78d4e182ee21fe78bf2fbe69711975ee2827cc
rafael13almeida <rafael13almeida@gmail.com>
2023-07-20 08:30:11 -0300

    Adicionando testes de ordenação e botão Dismiss

--- cypress/e2e/hacker-news.cy.js
+++ cypress/e2e/hacker-news.cy.js
@@ -36,7 +36,7 @@ describe('Hacker-news', () => {
         .should('contain', 'More')
     })
 
-    it('Verifica itens na página inicial', () => {
+    it('Verifica quantidade de itens na página inicial', () => {
       cy.get('.table-row')
         .should('have.length', 100)
     })
@@ -50,7 +50,7 @@ describe('Hacker-news', () => {
         .should('have.length', 100)
     })
 
-    it('Verifica quantos itens ao clicar no "More"', () => {
+    it('Verifica quantidade de itens ao clicar no "More"', () => {
       cy.get('.table-row')
         .should('have.length', 100)
       cy.get('.interactions button[type=button]')
@@ -64,29 +64,143 @@ describe('Hacker-news', () => {
   })
 
   context('Mock API', () => {
-    
     const itens = require('../fixtures/itens')
+    
     beforeEach(() => {
-
       cy.intercept(
         'GET',
         `**/search?query=${textoInicial}&page=0**`,
         { fixture: 'itens' }
-      ).as('getItens')
+      ).as('getItensFix')
 
       cy.visit('/')
-      cy.wait('@getItens')
+      cy.wait('@getItensFix')
     })
 
-    it('Verifica ordenação do grid', () => {
+    it('Verifica botão Dismiss', () => {
       cy.get('.table-row')
-        .first()
-        .should('contain', itens.hits[0].title)
-        .and('contain', itens.hits[0].author)
-        .and('contain', itens.hits[0].num_comments)
-        .and('contain', itens.hits[0].points)
-      cy.get(`.table-row a:contains(${itens.hits[0].title})`)
-        .should('have.attr', 'href', itens.hits[0].url)
+        .should('have.length', 2)
+      cy.contains('button', 'Dismiss')
+        .should('be.visible')
+        .click()
+      cy.get('.table-row')
+        .should('have.length', 1)
     })
+
+    context('Ordenação', () => {
+      it('Verifica ordenação por título', () => {
+        cy.get('.table-row')
+          .first()
+          .should('contain', itens.hits[0].title)
+          .and('contain', itens.hits[0].author)
+          .and('contain', itens.hits[0].num_comments)
+          .and('contain', itens.hits[0].points)
+        cy.get(`.table-row a:contains(${itens.hits[0].title})`)
+          .should('have.attr', 'href', itens.hits[0].url)
+  
+        cy.contains('button', 'Title').as('title')
+          .should('be.visible')
+          .click()
+        cy.get('.button-active')
+          .should('be.visible')
+  
+        cy.get('@title') 
+          .click()
+  
+        cy.get('.table-row')
+          .first()
+          .should('contain', itens.hits[1].title)
+          .and('contain', itens.hits[1].author)
+          .and('contain', itens.hits[1].num_comments)
+          .and('contain', itens.hits[1].points)
+        cy.get(`.table-row a:contains(${itens.hits[1].title})`)
+          .should('have.attr', 'href', itens.hits[1].url)
+      })
+
+      it('Verifica ordenação por Autor', () => {
+        cy.get('.table-row')
+          .first()
+          .should('contain', itens.hits[0].title)
+          .and('contain', itens.hits[0].author)
+          .and('contain', itens.hits[0].num_comments)
+          .and('contain', itens.hits[0].points)
+        cy.get(`.table-row a:contains(${itens.hits[0].title})`)
+          .should('have.attr', 'href', itens.hits[0].url)
+  
+        cy.contains('button', 'Author').as('autor')
+          .should('be.visible')
+          .click()
+        cy.get('.button-active')
+          .should('be.visible')
+  
+        cy.get('@autor') 
+          .click()
+  
+        cy.get('.table-row')
+          .first()
+          .should('contain', itens.hits[1].title)
+          .and('contain', itens.hits[1].author)
+          .and('contain', itens.hits[1].num_comments)
+          .and('contain', itens.hits[1].points)
+        cy.get(`.table-row a:contains(${itens.hits[1].title})`)
+          .should('have.attr', 'href', itens.hits[1].url)
+      })
+
+      it('Verifica ordenação por Comentarios', () => {
+        cy.get('.table-row')
+          .first()
+          .should('contain', itens.hits[0].title)
+          .and('contain', itens.hits[0].author)
+          .and('contain', itens.hits[0].num_comments)
+          .and('contain', itens.hits[0].points)
+        cy.get(`.table-row a:contains(${itens.hits[0].title})`)
+          .should('have.attr', 'href', itens.hits[0].url)
+  
+        cy.contains('button', 'Comments').as('comentarios')
+          .should('be.visible')
+          .click()
+        cy.get('.button-active')
+          .should('be.visible')
+
+        cy.get('.table-row')
+          .first()
+          .should('contain', itens.hits[1].title)
+          .and('contain', itens.hits[1].author)
+          .and('contain', itens.hits[1].num_comments)
+          .and('contain', itens.hits[1].points)
+        cy.get(`.table-row a:contains(${itens.hits[1].title})`)
+          .should('have.attr', 'href', itens.hits[1].url)
+      })
+
+      it('Verifica ordenação por Pontos', () => {
+        cy.get('.table-row')
+          .first()
+          .should('contain', itens.hits[0].title)
+          .and('contain', itens.hits[0].author)
+          .and('contain', itens.hits[0].num_comments)
+          .and('contain', itens.hits[0].points)
+        cy.get(`.table-row a:contains(${itens.hits[0].title})`)
+          .should('have.attr', 'href', itens.hits[0].url)
+  
+        cy.contains('button', 'Points').as('pontos')
+          .should('be.visible')
+          .click()
+        cy.get('.button-active')
+          .should('be.visible')
+  
+        cy.get('@pontos') 
+          .click()
+  
+        cy.get('.table-row')
+          .first()
+          .should('contain', itens.hits[1].title)
+          .and('contain', itens.hits[1].author)
+          .and('contain', itens.hits[1].num_comments)
+          .and('contain', itens.hits[1].points)
+        cy.get(`.table-row a:contains(${itens.hits[1].title})`)
+          .should('have.attr', 'href', itens.hits[1].url)
+      })
+    })
+   
   })
 })
